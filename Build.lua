1b43dde55b9e290427f0e7d20ef2ad5d46b56a99
An Spiteri <an@anspiteri.com>
2025-04-21 18:40:14 +1000

    build: added premake build options for subproject inclusion

--- Build.lua
+++ Build.lua
@@ -3,15 +3,37 @@ workspace "WaveKit"
    configurations { "Debug", "Release", "Dist" }
    startproject "Demo"
 
+   outputdir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+   
    -- Workspace-wide build options for MSVC
    filter "system:windows"
       buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+   filter {}
 
-OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+-- Custom build options
+newoption {
+   trigger = "with-demo",
+   description = "Include the application demo project"
+}
 
+newoption {
+   trigger = "with-tests",
+   description = "Include test suite"
+}
+
+-- Include subprojects
 group "Core"
 	include "Core/Build-Core.lua"
 group ""
 
-include "Tests/Build-Tests.lua"
-include "Demo/Build-Demo.lua"
+if _OPTIONS["with-demo"] then
+   group "Demo"
+      include "Demo/Build-Demo.lua"
+   group ""
+end
+
+if _OPTIONS["with-tests"] then
+   group "Tests"
+      include "Tests/Build-Tests.lua"
+   group ""
+end
